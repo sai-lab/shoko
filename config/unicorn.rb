e4093327f94432e8853ac6c16bf155f3929d78ad
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-03-03 19:44:38 +0900

    Unicornの設定ファイルを追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,34 @@
-worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+# -*- coding: utf-8 -*-
+# ワーカーの数
+worker_processes 2
+
+# タイムアウト
 timeout 15
+
+# ソケット
+listen  '/tmp/unicorn.sock'
+pid     '/tmp/unicorn.pid'
+
+# ログ
+log = '/var/log/rails/unicorn.log'
+stderr_path File.expand_path( 'log/unicorn.log', ENV['RAILS_ROOT'] )
+stdout_path File.expand_path( 'log/unicorn.log', ENV['RAILS_ROOT'] )
+
 preload_app true
+GC.respond_to?( :copy_on_write_friendly= ) and GC.copy_on_write_friendly = true
 
 before_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
-    Process.kill 'QUIT', Process.pid
-  end
+  defined?( ActiveRecord::Base ) and ActiveRecord::Base.connection.disconnect!
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  unless old_pid == server.pid
+    begin
+      Process.kill :QUIT, File.read( old_pid ).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
 
 after_fork do |server, worker|
-  Signal.trap 'TERM' do
-    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  defined?( ActiveRecord::Base ) and ActiveRecord::Base.establish_connection
 end
