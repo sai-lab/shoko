15058c362e0ea748d8a1afefe4a83159a112844b
Akihiko Horiuchi <12ff5b8@gmail.com>
2015-04-17 16:19:19 +0900

    Emailカラム追加時に空の場合の処理を追加

--- db/migrate/20150416101206_recoverable_users.rb
+++ db/migrate/20150416101206_recoverable_users.rb
@@ -1,11 +1,23 @@
 class RecoverableUsers < ActiveRecord::Migration
-  def change
-    change_table(:users) do |t|
-      t.string :email, null: false, default: ""
-      t.string :reset_password_token
-      t.datetime :reset_password_sent_at
+  def up
+    add_column :users, :email, :string, null: false, default: ""
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
+
+    User.all.each do |user|
+      if user.email.empty?
+        user.update email: "#{user.account}@example.com"
+      end
     end
 
     add_index :users, :email, unique: true
+  end
+
+  def down
+    remove_column :users, :email
+    remove_column :users, :reset_password_token
+    remove_column :users, :reset_password_sent_at
+
+    remove_index :users, :email
   end
 end
